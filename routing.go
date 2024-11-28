efdc1fd96f8eee6546f74497899e1993dad3948e
rume edgar <rumedevgar@gmail.com>
2024-11-28 21:57:31 +0100

    updated routing

--- routing.go
+++ routing.go
@@ -1,19 +1,19 @@
 package main
 
 func xorDistance(a, b uint32) uint32 {
-	return a ^ b
+    return a ^ b
 }
 
 func (n *Node) FindClosest(targetID uint32) *Node {
-	var closest *Node
-	minDistance := uint32(^uint32(0)) // Max uint32 value
+    var closest *Node
+    minDistance := uint32(^uint32(0)) // Max uint32 value
 
-	for _, neighbor := range n.Neighbors {
-		distance := xorDistance(neighbor.ID, targetID)
-		if distance < minDistance {
-			minDistance = distance
-			closest = &neighbor
-		}
-	}
-	return closest
+    for _, neighbor := range n.Neighbors {
+        distance := xorDistance(neighbor.ID, targetID)
+        if distance < minDistance {
+            minDistance = distance
+            closest = &neighbor
+        }
+    }
+    return closest
 }
