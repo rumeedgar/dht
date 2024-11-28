753242b4d400257011a144b6ea45459addbc1a4a
rume edgar <rumedevgar@gmail.com>
2024-11-28 22:05:48 +0100

    updated hashing logic

--- hash.go
+++ hash.go
@@ -9,5 +9,5 @@ func hashKey(key string) uint32 {
 	hasher := sha1.New()
 	hasher.Write([]byte(key))
 	hashBytes := hasher.Sum(nil)
-	return binary.BigEndian.Uint32(hashBytes[:4]) // Take the first 4 bytes
+	return binary.BigEndian.Uint32(hashBytes[:4])
 }
