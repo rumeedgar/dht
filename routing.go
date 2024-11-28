ef8ededacbb99e5582de760aece11d6657669096
rume edgar <rumedevgar@gmail.com>
2024-11-28 21:34:58 +0100

    using XOR distance to determine proximity between nodes

---
+++ routing.go
@@ -0,0 +1,19 @@
+package main
+
+func xorDistance(a, b uint32) uint32 {
+	return a ^ b
+}
+
+func (n *Node) FindClosest(targetID uint32) *Node {
+	var closest *Node
+	minDistance := uint32(^uint32(0)) // Max uint32 value
+
+	for _, neighbor := range n.Neighbors {
+		distance := xorDistance(neighbor.ID, targetID)
+		if distance < minDistance {
+			minDistance = distance
+			closest = &neighbor
+		}
+	}
+	return closest
+}
