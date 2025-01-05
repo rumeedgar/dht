0abaefd8dacc19ac1c3dd83adf78c7e05a298919
rume edgar <rumedevgar@gmail.com>
2025-01-05 19:39:45 +0100

    fixed hashing bug

--- hash.go
+++ hash.go
@@ -1,13 +1,13 @@
-package main
+ package main
 
 import (
-	"crypto/sha1"
-	"math/big"
+    "crypto/sha1"
+    "math/big"
 )
 
 // hashKey takes a key (string) and returns its hash as a big integer
 func hashKey(key string) *big.Int {
-	h := sha1.New()
-	h.Write([]byte(key))
-	return new(big.Int).SetBytes(h.Sum(nil))
+    h := sha1.New()
+    h.Write([]byte(key))
+    return new(big.Int).SetBytes(h.Sum(nil))
 }
